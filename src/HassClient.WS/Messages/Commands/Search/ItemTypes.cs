c77bdd7de80ba97ed00d0959a507bae5c158d7de
vicfergar <victor@revenuecat.com>
2024-11-02 18:06:50 +0100

    feat: Update SearchApi

--- src/HassClient.WS/Messages/Commands/Search/ItemTypes.cs
+++ src/HassClient.WS/Messages/Commands/Search/ItemTypes.cs
@@ -1,48 +1,78 @@
 ﻿namespace HassClient
 {
     /// <summary>
-    /// Well known Home Assistant item types used during relation search.
+    /// Represents well-known Home Assistant item types used during relation search.
     /// </summary>
     public enum ItemTypes
     {
         /// <summary>
-        /// Physical areas of a home.
+        /// Represents physical areas within a home.
         /// </summary>
         Area,
 
         /// <summary>
-        /// Home Assistant automation.
+        /// Represents a Home Assistant automation.
         /// </summary>
         Automation,
 
         /// <summary>
-        /// Configuration data that are persistently stored by Home Assistant.
+        /// Represents a blueprint for creating automations in Home Assistant.
+        /// </summary>
+        AutomationBlueprint,
+
+        /// <summary>
+        /// Represents configuration data stored persistently by Home Assistant.
         /// </summary>
         ConfigEntry,
 
         /// <summary>
-        /// Home Assistant device.
+        /// Represents a device in Home Assistant.
         /// </summary>
         Device,
 
         /// <summary>
-        /// Home Assistant entity.
+        /// Represents an entity in Home Assistant.
         /// </summary>
         Entity,
 
         /// <summary>
-        /// Home Assistant group.
+        /// Represents a floor within a home.
+        /// </summary>
+        Floor,
+
+        /// <summary>
+        /// Represents a group of items in Home Assistant.
         /// </summary>
         Group,
 
         /// <summary>
-        /// Home Assistant scene.
+        /// Represents an integration in Home Assistant.
+        /// </summary>
+        Integration,
+
+        /// <summary>
+        /// Represents a label used in Home Assistant.
+        /// </summary>
+        Label,
+
+        /// <summary>
+        /// Represents a person in Home Assistant.
+        /// </summary>
+        Person,
+
+        /// <summary>
+        /// Represents a scene in Home Assistant.
         /// </summary>
         Scene,
 
         /// <summary>
-        /// Home Assistant script.
+        /// Represents a script in Home Assistant.
         /// </summary>
         Script,
+
+        /// <summary>
+        /// Represents a blueprint for creating scripts in Home Assistant.
+        /// </summary>
+        ScriptBlueprint,
     }
 }
